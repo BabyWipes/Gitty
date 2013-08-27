7aed6e288e8ff536fb621e783f977763108192eb
Yuzko <tomperry1997@gmail.com>
2013-08-27 21:59:48 +1000

    add base and commands

---
+++ src/me/yuzko/gitty/Commands.java
@@ -0,0 +1,17 @@
+package me.yuzko.gitty;
+
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+public class Commands {
+
+	public static boolean method(Player sender, Command cmd, String lowerCase,
+			String[] args, String arg) {
+		if(arg.equals("search") || arg.equals("find"))
+		{
+			
+		}
+		return false;
+	}
+
+}
