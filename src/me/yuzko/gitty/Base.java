86145aaa8d1dbdd2e7b6c216897785dd7b756950
Yuzko <tomperry1997@gmail.com>
2013-08-27 20:55:10 +1000

    swagtest

---
+++ src/me/yuzko/gitty/Base.java
@@ -0,0 +1,12 @@
+package me.yuzko.gitty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sun.istack.internal.logging.Logger;
+
+public class Base extends JavaPlugin
+{
+	public static Base plugin;
+	public static Logger logger = Logger.getLogger("Minecraft");
+
+}
