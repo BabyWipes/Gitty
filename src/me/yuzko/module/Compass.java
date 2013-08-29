158190898369492cb70053600ef127c6cb0ef36d
Yuzko <ow_oh_kiss_liss3456@hotmail.com>
2013-08-29 20:46:50 +1000

    Update Compass.java

--- src/me/yuzko/module/Compass.java
+++ src/me/yuzko/module/Compass.java
@@ -1,5 +1,9 @@
 package me.yuzko.module;
 
+import java.util.WeakHashMap;
+
+import me.yuzko.gitty.Base;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -7,11 +11,22 @@ import org.bukkit.entity.Player;
 
 public class Compass {
 
-	public Compass
+	private Compass(String plugin)
 	{
+		super();
 	}
 
-	public static void command(Player caller, String[] args) {
+
+	public static Compass get()
+	{
+		if(instance == null)
+			instance = new Compass(Base.name);
+		return instance;
+	}
+
+	public static void command(Player caller, String[] args) 
+	{
+
 		if(args.length < 1)
 		{
 			caller.sendMessage(ChatColor.DARK_RED + "[Compass] " + ChatColor.RED 
@@ -26,24 +41,24 @@ public class Compass {
 			}
 			else
 			{
-				if(!(caller.getInventory().getItemInHand().getTypeId() == /compassTODO))
+				if(!(caller.getInventory().getItemInHand().getTypeId() == 347))
 				{
 					caller.sendMessage(ChatColor.DARK_RED + "[Compass]" + ChatColor.RED 
-					+ ChatColor.RED + "You need to be holding a compass to use this command);
+							+ ChatColor.RED + "You need to be holding a compass to use this command");
 				}
 				else
 				{
 
-				int x = Bukkit.getPlayer(args[0]).getLocation().getBlockX();
-				int y = Bukkit.getPlayer(args[0]).getLocation().getBlockY();
-				int z = Bukkit.getPlayer(args[0]).getLocation().getBlockZ();
-				caller.sendMessage(ChatColor.DARK_RED + "[Compass] " + ChatColor.LIGHT_PURPLE
-						+ args[0] + ChatColor.GREEN + " is located at; " + ChatColor.DARK_GREEN + x 
-						+ ChatColor.GREEN + ", " + ChatColor.DARK_GREEN + y + ChatColor.GREEN + 
-						", " + ChatColor.DARK_GREEN + z);
+					int x = Bukkit.getPlayer(args[0]).getLocation().getBlockX();
+					int y = Bukkit.getPlayer(args[0]).getLocation().getBlockY();
+					int z = Bukkit.getPlayer(args[0]).getLocation().getBlockZ();
+					caller.sendMessage(ChatColor.DARK_RED + "[Compass] " + ChatColor.LIGHT_PURPLE
+							+ args[0] + ChatColor.GREEN + " is located at; " + ChatColor.DARK_GREEN + x 
+							+ ChatColor.GREEN + ", " + ChatColor.DARK_GREEN + y + ChatColor.GREEN + 
+							", " + ChatColor.DARK_GREEN + z);
 				}
 			}
 		}
 	}
-
+	public static Compass instance = null;
 }
