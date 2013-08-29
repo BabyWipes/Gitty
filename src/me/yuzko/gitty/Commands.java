b8c6f101567bbe8b44315d716dd2788434ba8df8
Yuzko <ow_oh_kiss_liss3456@hotmail.com>
2013-08-29 20:42:24 +1000

    Update Commands.java

--- src/me/yuzko/gitty/Commands.java
+++ src/me/yuzko/gitty/Commands.java
@@ -1,6 +1,7 @@
 package me.yuzko.gitty;
 
 import me.yuzko.module.Compass;
+import me.yuzko.module.DonatorChat;
 
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
@@ -13,9 +14,16 @@ public class Commands {
 
 	public static boolean method(Player caller, Command cmd, String arg, String args[])
 	{
-		if(arg.equals("search") || arg.equals("find"))
+		if(arg.equalsIgnoreCase("search") || arg.equals("find"))
 		{
 			Compass.command(caller, args);
+		}
+		else
+		{
+			if(arg.equalsIgnoreCase("dc") || arg.equalsIgnoreCase("dm"))
+			{
+				DonatorChat.command(caller, args);
+			}
 		}
 		return false;
 	}
