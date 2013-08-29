59f1c5da211aa9d51565bbc4133563abcc495725
Yuzko <ow_oh_kiss_liss3456@hotmail.com>
2013-08-29 20:47:10 +1000

    Create DonatorChat.java

---
+++ src/me/yuzko/module/DonatorChat.java
@@ -0,0 +1,61 @@
+package me.yuzko.module;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import me.yuzko.gitty.Base;
+
+public class DonatorChat 
+{
+
+	private DonatorChat(String plugin)
+	{
+		super();
+	}
+
+	public static DonatorChat get()
+	{
+		if(instance == null)
+			instance = new DonatorChat(Base.name);
+		return instance;
+	}
+
+	public static void command(Player caller, String[] args)
+	{
+		
+		String msg = args[0];
+		
+		if(args.length < 1)
+		{
+			caller.sendMessage(ChatColor.DARK_RED + "[DChat] " + ChatColor.RED 
+					+ "Invalid command");
+		}
+		else
+		{
+			if(!(caller.hasPermission("donator.chat")))
+			{
+				caller.sendMessage(ChatColor.DARK_RED + "DChat " + ChatColor.RED 
+						+ "You need to be a donator to use this command");
+			}
+			else
+			{
+				Player[] players = Bukkit.getOnlinePlayers();
+				for(Player p : players)
+				{
+					if(!(p.hasPermission("donator.chat")))
+					{
+						//DO NOTHING
+					}
+					else
+					{
+						p.sendMessage(msg);
+					}
+				}
+			}
+		}
+	}
+
+	public static DonatorChat instance = null;
+
+}
